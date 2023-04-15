262218972189b2ebe444325fc45256bf3591ea3a
catg2265 <cgrave20@student.aau.dk>
2023-04-15 12:16:50 +0200

    Ground collision detection

--- Land Party/Assets/Scripts/PlayerController.cs
+++ Land Party/Assets/Scripts/PlayerController.cs
@@ -15,16 +15,19 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField] private List<Sprite> _playersprites = new List<Sprite>();
 
+    [SerializeField] private Transform GroundCheck;
+    
     public int playerID = 0;
 
     [SerializeField] private float speed = 5f;
+
+    [SerializeField] private float jumpPower = 15f;
 
     private float horizontal;
 
     public bool activateSpriteSelec = false;
 
-
-    public LayerMask LayerMask;
+    public LayerMask groundLayer;
 
     private bool touchGround = false;
     
@@ -46,10 +49,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontal = input.actions["Move"].ReadValue<Vector2>().x;
+        if (horizontal > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else
+        {
+            _spriteRenderer.flipX = true;
+        }
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y); 
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(GroundCheck.position, 0.2f, groundLayer);
     }
 }
