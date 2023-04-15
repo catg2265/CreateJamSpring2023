36beb3335edaa437448a276f8bc1aa68202f2e5d
catg2265 <cgrave20@student.aau.dk>
2023-04-15 10:36:23 +0200

    Tags added plus some movement

--- Land Party/Assets/Scripts/PlayerController.cs
+++ Land Party/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,13 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private Rigidbody2D rb;
 
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    
     [SerializeField] private List<Sprite> _playersprites = new List<Sprite>();
+
+    [SerializeField] private float speed = 5f;
+
+    private float horizontal;
 
     public bool activateSpriteSelec = false;
     // Start is called before the first frame update
@@ -18,13 +25,18 @@ public class PlayerController : MonoBehaviour
         if (activateSpriteSelec)
         {
             GameObject[] goArr = GameObject.FindGameObjectsWithTag("Player");
-            
+            _spriteRenderer.sprite = _playersprites[goArr.Length];
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = input.actions["Move"].ReadValue<Vector2>().x;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + new Vector2(horizontal, rb.velocity.y) * (speed*Time.fixedDeltaTime));
     }
 }
