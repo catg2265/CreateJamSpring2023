95968b52e989d76766bdeb8fc60b8ab227149eae
BicaniWolfie <mathiasj20@live.dk>
2023-04-15 15:52:50 +0200

    DD Arr' progress

--- Land Party/Assets/Scripts/DDRPlayerController.cs
+++ Land Party/Assets/Scripts/DDRPlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,63 @@ using UnityEngine.InputSystem;
 
 public class DDRPlayerController : MonoBehaviour
 {
+    public int playerNumber;
+    public int pointCount;
+    public GameObject[] inputSpheres;
+    public MiniGameManager miniGameManager;
+
+    private bool _hasPressedThisBeat;
     [SerializeField] private PlayerInput input;
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        Debug.Log(BPMManager.instance.GetBPM());
+        BPMManager.BeatTimerEvent.AddListener(OnBeatUpdate);
     }
 
-    // Update is called once per frame
+    void OnBeatUpdate(GameObject o)
+    {
+        _hasPressedThisBeat = false;
+    }
+    
     void Update()
     {
-        if (input.actions["DDR"].WasPressedThisFrame())
-        {
-            Debug.Log(input.actions["DDR"].ReadValue<Vector2>());
-        }
+        if (!input.actions["DDR"].WasPressedThisFrame())
+            return;
         
+        Vector2 curr = input.actions["DDR"].ReadValue<Vector2>();
+        if (_hasPressedThisBeat || curr is { x: 0f, y: 0 })
+            return;
+        _hasPressedThisBeat = true;
+
+        switch (curr)
+        {
+            case {x:1,y:0}:
+                miniGameManager.HitBeat(this,BeatInput.LEFT);
+                return;
+            case {x:-1,y:0}:
+                miniGameManager.HitBeat(this,BeatInput.RIGHT);
+                return;
+            case {x:0,y:1}:
+                miniGameManager.HitBeat(this,BeatInput.UP);
+                return;
+            case {x:0,y:-1}:
+                miniGameManager.HitBeat(this,BeatInput.DOWN);
+                return;
+        }
+
+        bool pressedRight = curr.x > 0;
+        bool pressedUp = curr.y > 0;
+
+        if (pressedRight && pressedUp)
+            miniGameManager.HitBeat(this, BeatInput.UPRIGHT);
+        else if (pressedUp)
+            miniGameManager.HitBeat(this, BeatInput.UPLEFT);
+        else if (pressedRight)
+            miniGameManager.HitBeat(this, BeatInput.DOWNRIGHT);
+        else
+            miniGameManager.HitBeat(this, BeatInput.DOWNLEFT);
     }
 }
--- Land Party/Assets/Scripts/MiniGameManager.cs
+++ Land Party/Assets/Scripts/MiniGameManager.cs
@@ -1,34 +1,63 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
+using Object = UnityEngine.Object;
 
 [Serializable]
 public class Beat
 {
-    public bool[] hitNotes = { false, false, false, false };
-    private bool[] _playerPressed = { false, false, false, false };
+    public BeatInput hitNote;
     public int hitBeat;
 }
 
 public class MiniGameManager : MonoBehaviour
 {
     public List<Beat> beats = new();
+    public Object NotePrefab;
+    
     public AudioSource song;
 
+    private List<int> _noteBuffer;
     private int _songBeatIndex;
     private int _currBeat;
 
     // Start is called before the first frame update
     void Start()
     {
-        BPMManager.instance.beatTimerEvent.AddListener(NewBeat);
+        BPMManager.BeatTimerEvent.AddListener(NewBeat);
+        _noteBuffer = beats.Select(b => b.hitBeat).ToList();
     }
 
+    public void HitBeat(DDRPlayerController player, BeatInput input)
+    {
+        if(beats[0].hitNote == input)
+            player.pointCount += CalculateScore();
+        beats.RemoveAt(0);
+    }
+
+    private int CalculateScore()
+    {
+        int beatDelta = Math.Abs(beats[0].hitBeat - _currBeat);
+
+        if (beatDelta == 0)
+            return 10;
+        if (beatDelta < 2)
+            return 5;
+        if (beatDelta < 5)
+            return 2;
+        return 0;
+    }
+    
     private void NewBeat(GameObject o)
     {
         _currBeat += 1;
+        if (_noteBuffer.Remove(_currBeat + 1))
+        {
+            Object newObject = Instantiate(NotePrefab);
+            newObject.GameObject().transform.position -= new Vector3(0,3, 0);
+            newObject.GetComponent<ScrollingNoteInstance>().enableNotes(beats.Find(b => b.hitBeat == _currBeat+1).hitNote);
+        }
     }
 }
